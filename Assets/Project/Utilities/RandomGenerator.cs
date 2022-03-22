74ca6608da590e58fbc7e4c50f5485f7a8738fbe
tahersamadi <tahers@doublecoconut.com>
2022-03-22 22:08:55 +0400

    feat(Utilities): add RandomGenerator.cs to get random online

---
+++ Assets/Project/Utilities/RandomGenerator.cs
@@ -0,0 +1,21 @@
+using Codice.Client.BaseCommands.BranchExplorer;
+using Cysharp.Threading.Tasks;
+using Project.Model;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Project.Utilities
+{
+    public static class RandomGenerator
+    {
+        public static async UniTask<int> GetRandomChoice()
+        {
+            var txt = (await UnityWebRequest.Get
+                ("http://www.randomnumberapi.com/api/v1.0/random?min=0&max=2&count=1")
+                .SendWebRequest())
+                .downloadHandler.text;
+            var index = int.Parse(txt.Substring(1,1));
+            return index;
+        }
+    }
+}
