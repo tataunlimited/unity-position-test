bdac1c378997d15636ab4e12c519ee2e02bdeaff
tahersamadi <tahers@doublecoconut.com>
2022-03-23 01:05:15 +0400

    feat: adding Symbols class to store sprite in SymbolPresenter.cs

--- Assets/Project/UI/SymbolPresenters.cs
+++ Assets/Project/UI/SymbolPresenters.cs
@@ -1,12 +1,50 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
+using Project.Model;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Project.UI
 {
     public abstract class SymbolPresenter
     {
-        protected SymbolPresenter(Image image)
+        private readonly Image _image;
+        private readonly Symbols _symbols;
+        
+        protected SymbolPresenter(Image image, Symbols symbols)
         {
+            _image = image;
+            _symbols = symbols;
+        }
+
+        public void SetSymbol(Hand hand)
+        {
+            _image.sprite = _symbols.GetSymbol(hand);
+        }
+
+        public void Reset()
+        {
+            _image.sprite = _symbols.questionMark;
+        }
+        
+        [Serializable]
+        public class Symbols
+        {
+            public Sprite rock;
+            public Sprite paper;
+            public Sprite scissors;
+            public Sprite questionMark;
+
+            public Sprite GetSymbol(Hand hand)
+            {
+                return hand switch
+                {
+                    Hand.Rock => rock,
+                    Hand.Paper => paper,
+                    Hand.Scissors => scissors,
+                    _ => throw new ArgumentOutOfRangeException(nameof(hand), hand, null)
+                };
+            }
         }
     }
 
