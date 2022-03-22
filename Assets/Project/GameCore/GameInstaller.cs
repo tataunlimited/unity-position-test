daa9870e17eee0c929a46ff7a4b2e0035866aa82
tahersamadi <tahers@doublecoconut.com>
2022-03-23 03:13:12 +0400

    feat(GameInstaller): update GameInstaller.cs.
     - bind GameController
    
     - declare Signals
    
     - bind Signals

--- Assets/Project/GameCore/GameInstaller.cs
+++ Assets/Project/GameCore/GameInstaller.cs
@@ -1,5 +1,6 @@
 using Project.Model;
-using UnityEngine;
+using Project.Signals;
+using Project.UI;
 using Zenject;
 
 namespace Project.GameCore
@@ -9,6 +10,38 @@ namespace Project.GameCore
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<RpsModel>().AsSingle().NonLazy();
+            Container.BindInterfacesAndSelfTo<GameController>().AsSingle().NonLazy();
+            InstallSignals();
+            BindSignals();
+        }
+
+        
+
+        private void InstallSignals()
+        {
+            SignalBusInstaller.Install(Container);
+            Container.DeclareSignal<PlayerSubmitSignal>();
+            Container.DeclareSignal<ReadyForCountDownSignal>();
+            Container.DeclareSignal<PlayerSymbolReadySignal>();
+            Container.DeclareSignal<OpponentSymbolReadySignal>();
+            Container.DeclareSignal<GameOverSignal>();
+            Container.DeclareSignal<ResetGameSignal>();
+        }
+        
+        private void BindSignals()
+        {
+            Container.BindSignal<PlayerSubmitSignal>()
+                .ToMethod<GameController>((gc, signal) => gc.ValidateState(signal.PlayerModel));
+            Container.BindSignal<ReadyForCountDownSignal>()
+                .ToMethod<CountdownPresenter>((cp, signal) => cp.StartCountDown(signal.Seconds)).FromResolve();
+            Container.BindSignal<PlayerSymbolReadySignal>()
+                .ToMethod<PlayerSymbolPresenter>((ps, signal) => { ps.SetSymbol(signal.Hand); }).FromResolve();
+            Container.BindSignal<OpponentSymbolReadySignal>()
+                .ToMethod<OpponentSymbolPresenter>((os, signal) => { os.SetSymbol(signal.Hand); }).FromResolve();
+            Container.BindSignal<GameOverSignal>()
+                .ToMethod<ResultTextPresenter>((rt, signal) => { rt.ShowResults(signal.Outcome); }).FromResolve();
+            Container.BindSignal<ResetGameSignal>().ToMethod<IResettable>(x => x.Reset).FromResolveAll();
+
         }
     }
 }
